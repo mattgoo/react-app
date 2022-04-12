1d0dba22fab6609829b2bb9f79dc662466fcfc50
Matt Goodwin <118goodwin@gmail.com>
2022-04-12 17:56:01 -0400

    styled

--- src/App.js
+++ src/App.js
@@ -4,30 +4,38 @@ import galleryImg from './images/galleryImg.png';
 import projectsImg from './images/projectsImg.png';
 
 
-
+import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 
 function App() {
   return (
-    <div className='App'>
-      <Grid className='grid-container' container spacing={12}>
-        <Grid className='grid-inner' href='/info' item xs={4} sx={{
-          backgroundImage: `url(${infoImg})`,
-        }}>
-          <h1 className='big-Text'>Info</h1>
+    <div>
+      <div className='App'>
+        <Grid className='grid-container' container spacing={12}>
+          <Grid className='grid-inner' href="https://google.com" item xs={4} sx={{
+            backgroundImage: `url(${infoImg})`,
+          }}>
+            <h1 className='big-Text'>Info</h1>
+          </Grid>
+          <Grid className='grid-inner' item xs={4} sx={{
+            backgroundImage: `url(${galleryImg})`,
+          }}>
+            <h1 className='big-Text'>Gallery</h1>
+          </Grid>
+          <Grid className='grid-inner' item xs={4} sx={{
+            backgroundImage: `url(${projectsImg})`,
+          }}>
+            <h1 className='big-Text'>Projects</h1>
+          </Grid>
         </Grid>
-        <Grid className='grid-inner' item xs={4} sx={{
-          backgroundImage: `url(${galleryImg})`,
-        }}>
-          <h1 className='big-Text'>Gallery</h1>
-        </Grid>
-        <Grid className='grid-inner' item xs={4} sx={{
-          backgroundImage: `url(${projectsImg})`,
-        }}>
-          <h1 className='big-Text'>Projects</h1>
-        </Grid>
-      </Grid>
+      </div>
+      <div>
+        <Link href='https://google.com'>
+          <h1>Google</h1>
+        </Link>
+      </div>
     </div>
+
   );
 }
 
