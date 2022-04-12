151a658a1ea315db2691b053da6b891da8bf1e3f
Matt Goodwin <118goodwin@gmail.com>
2022-04-12 17:11:44 -0400

    added a background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Grid from '@mui/material/Grid';
@@ -8,12 +7,12 @@ function App() {
     <div className='App'>
       <Grid className='grid-container' container spacing={12}>
         <Grid className='grid-inner' item xs={4}>
-          <h1 className='big-Text' >Info</h1>
+          <h1 className='big-Text'>Info</h1>
         </Grid>
         <Grid className='grid-inner' item xs={4}>
           <h1 className='big-Text'>Gallery</h1>
         </Grid>
-        <Grid className='grid-inner' item xs={4}>
+        <Grid className='grid-inner' item xs={4} >
           <h1 className='big-Text'>Projects</h1>
         </Grid>
       </Grid>
