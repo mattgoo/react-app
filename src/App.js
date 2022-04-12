5419bbd5b679c5f6991f5cdb1f6cdc0298e27dcb
Matt Goodwin <118goodwin@gmail.com>
2022-04-12 17:44:27 -0400

    added funsies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import infoImg from './images/infoImg.png';
+import galleryImg from './images/galleryImg.png';
+import projectsImg from './images/projectsImg.png';
+
+
 
 import Grid from '@mui/material/Grid';
 
@@ -6,13 +11,19 @@ function App() {
   return (
     <div className='App'>
       <Grid className='grid-container' container spacing={12}>
-        <Grid className='grid-inner' item xs={4}>
+        <Grid className='grid-inner' href='/info' item xs={4} sx={{
+          backgroundImage: `url(${infoImg})`,
+        }}>
           <h1 className='big-Text'>Info</h1>
         </Grid>
-        <Grid className='grid-inner' item xs={4}>
+        <Grid className='grid-inner' item xs={4} sx={{
+          backgroundImage: `url(${galleryImg})`,
+        }}>
           <h1 className='big-Text'>Gallery</h1>
         </Grid>
-        <Grid className='grid-inner' item xs={4} >
+        <Grid className='grid-inner' item xs={4} sx={{
+          backgroundImage: `url(${projectsImg})`,
+        }}>
           <h1 className='big-Text'>Projects</h1>
         </Grid>
       </Grid>
