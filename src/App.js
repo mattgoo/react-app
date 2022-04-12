c07db74c3f8a8b57eeabb93092ea014a7b78f766
Matt Goodwin <118goodwin@gmail.com>
2022-04-12 18:54:27 -0400

    added in progress

--- src/App.js
+++ src/App.js
@@ -2,36 +2,60 @@ import './App.css';
 import infoImg from './images/infoImg.png';
 import galleryImg from './images/galleryImg.png';
 import projectsImg from './images/projectsImg.png';
-
+import React from 'react';
 
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 
-
 function App() {
+  const [InfoTitle, setInfoTitle] = React.useState('Info');
+  const [GalleryTitle, setGalleryTitle] = React.useState('Gallery');
+  const [ProjectsTitle, setProjectsTitle] = React.useState('Projects');
+
+  // function to change the title of the page to in progress on hover
+  function changeInfo() {
+    if (InfoTitle === 'Info') {
+      setInfoTitle('In Progress');
+    }
+    else {
+      setInfoTitle('Info');
+    }
+  } 
+
+  function changeGallery() {
+    if (GalleryTitle === 'Gallery') {
+      setGalleryTitle('In Progress');
+    }
+    else {
+      setGalleryTitle('Gallery');
+    }
+  }
+
+
   const goToMyGithub = () => {
     window.open('https://github.com/mattgoo');
   };
+
+  
 
   return (
     <div>
       <div className='App'>
         <Grid className='grid-container' container spacing={12}>
-          <Grid className='grid-inner' item xs={4} sx={{
-            backgroundImage: `url(${infoImg})`,
-          }}>
-            <h1 className='big-Text'>Info</h1>
-            <p className='small-Text'>(In Progress)</p>
+          <Grid className='grid-inner' 
+                item xs={4} 
+                sx={{ backgroundImage: `url(${infoImg})`}} 
+                onMouseEnter={changeInfo}
+                onMouseLeave={changeInfo}>
+            <h2 className='big-Text'>{InfoTitle}</h2>
           </Grid>
-          <Grid className='grid-inner' item xs={4} sx={{
-            backgroundImage: `url(${galleryImg})`,
-          }}>
-            <h1 className='big-Text'>Gallery</h1>
-            <p className='small-Text'>(In Progress)</p>
+          <Grid className='grid-inner' 
+                item xs={4} sx={{ backgroundImage: `url(${galleryImg})`}}
+                onMouseEnter={changeGallery}
+                onMouseLeave={changeGallery}>
+            <h2 className='big-Text'>{GalleryTitle}</h2>
           </Grid>
-          <Grid className='grid-inner' onClick={goToMyGithub} item xs={4} sx={{
-            backgroundImage: `url(${projectsImg})`,
-          }}>
+          <Grid className='grid-inner' onClick={goToMyGithub} item xs={4} sx={{ backgroundImage: `url(${projectsImg})`}}>
             <h1 className='big-Text'>Projects</h1>
           </Grid>
         </Grid>
