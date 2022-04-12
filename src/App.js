5866ab0fd2b7ab7d4b06b8877160b3945e2e8126
Matt Goodwin <118goodwin@gmail.com>
2022-04-12 18:15:28 -0400

    add git

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import projectsImg from './images/projectsImg.png';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 
+
 function App() {
+  const goToMyGithub = () => {
+    window.open('https://github.com/mattgoo');
+  };
+
   return (
     <div>
       <div className='App'>
         <Grid className='grid-container' container spacing={12}>
-          <Grid className='grid-inner' href="https://google.com" item xs={4} sx={{
+          <Grid className='grid-inner' item xs={4} sx={{
             backgroundImage: `url(${infoImg})`,
           }}>
             <h1 className='big-Text'>Info</h1>
@@ -22,17 +27,12 @@ function App() {
           }}>
             <h1 className='big-Text'>Gallery</h1>
           </Grid>
-          <Grid className='grid-inner' item xs={4} sx={{
+          <Grid className='grid-inner' onClick={goToMyGithub} item xs={4} sx={{
             backgroundImage: `url(${projectsImg})`,
           }}>
             <h1 className='big-Text'>Projects</h1>
           </Grid>
         </Grid>
-      </div>
-      <div>
-        <Link href='https://google.com'>
-          <h1>Google</h1>
-        </Link>
       </div>
     </div>
 
