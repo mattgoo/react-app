f3f52c5f01c5c4cc71c82e9727c9525853a538c3
Matt Goodwin <118goodwin@gmail.com>
2022-05-05 20:02:48 -0400

    up styles

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -2,6 +2,8 @@ import '../App.css';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 
+import '../styles/ProjectsPage.css';
+
 // back button 
 import IconButton from '@mui/material/IconButton';
 import HomeIcon from '@mui/icons-material/Home';
@@ -29,10 +31,10 @@ function Projects() {
             <IconButton onClick={goToHome}>
                 <HomeIcon className='white-home' />
             </IconButton>
-            <h1 className='aboutText'>
+            <h1 className='git-title'>
                 Githubs
             </h1>
-            <div className='center-on-screen'>
+            <div className='center-projects'>
                 <Grid container spacing={2} className='info-grid'>
                     <Grid item xs={12} sm={6} onClick={() => goToMyGithub("personal")}>
                         <div className='git-page'>
